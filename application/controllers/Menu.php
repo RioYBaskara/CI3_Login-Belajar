a42570128936af95c69c4d19ddb29c11bf6b888e
Rio Yudha Baskara <156874101+RioYBaskara@users.noreply.github.com>
2024-07-11 15:05:45 +0700

    User Access, menggunakan function helper

--- application/controllers/Menu.php
+++ application/controllers/Menu.php
@@ -16,9 +16,7 @@ class Menu extends CI_Controller
     public function __construct()
     {
         parent::__construct();
-        if (!$this->session->userdata('emaiil')) {
-            redirect('auth');
-        }
+        is_logged_in();
     }
 
     // menu
--- application/controllers/User.php
+++ application/controllers/User.php
@@ -15,9 +15,7 @@ class User extends CI_Controller
     public function __construct()
     {
         parent::__construct();
-        if (!$this->session->userdata('emaiil')) {
-            redirect('auth');
-        }
+        is_logged_in();
     }
 
     public function index()
